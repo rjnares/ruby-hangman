5f96dc0b6e5fff413d0c887b4e1e7c7b7a6b2198
Roberto Nares <roberto.nares@gmail.com>
2024-07-24 22:59:09 -0700

    Create game i/o module and implement functionality that gets next valid letter guess from human player

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'host'
+require_relative 'game_io'
 
 # Game class
 class Game
+  include GameIO
+
   def initialize
     @host = Host.new
     @secret_word_array = @host.choose_random_word.split('')
     @secret_word_progress_array = Array.new(@secret_word_array.length, '_')
+    @incorrect_letters = []
+    @incorrect_guesses_left = 7
   end
 
   def play
-    puts 'Game is being played'
+    letter = next_letter(@secret_word_progress_array, @incorrect_letters, @incorrect_guesses_left)
+    p letter
   end
 end
---
+++ lib/game_io.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Game I/O module
+module GameIO
+  def next_guess
+    print 'Next guess: '
+    gets.chomp.downcase
+  end
+
+  def display_turn_text(word_progress_array, incorrect_letters, incorrect_guesses_left)
+    puts "Word to guess: #{word_progress_array.join(' ')}"
+    puts "Incorrect letters: #{incorrect_letters.join(' ')}"
+    puts "Incorrect guesses left: #{incorrect_guesses_left}"
+    puts
+  end
+
+  def invalid_input_warning
+    puts 'All guesses must be a single english letter...please try again'
+    puts
+  end
+
+  def previous_guess_warning(guess)
+    puts "The letter '#{guess}' has already been guessed...please try again"
+    puts
+  end
+
+  def next_letter(word_progress_array, incorrect_letters, incorrect_guesses_left)
+    display_turn_text(word_progress_array, incorrect_letters, incorrect_guesses_left)
+    next_guess_loop(word_progress_array, incorrect_letters)
+  end
+
+  def next_guess_loop(word_progress_array, incorrect_letters)
+    loop do
+      guess = next_guess
+      if invalid_input?(guess)
+        invalid_input_warning
+      elsif previous_guess?(guess, incorrect_letters, word_progress_array)
+        previous_guess_warning(guess)
+      else
+        return guess
+      end
+    end
+  end
+
+  def invalid_input?(input)
+    !(input.length == 1 && input.between?('a', 'z'))
+  end
+
+  def previous_guess?(letter, incorrect_letters, word_progress_array)
+    incorrect_letters.include?(letter) || word_progress_array.include?(letter)
+  end
+end
