a242a0c207a075b854ecc17ed2dd9f2baef3cca4
Roberto Nares <roberto.nares@gmail.com>
2024-07-24 21:09:11 -0700

    Move secret word state from host class to game class

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,8 @@ require_relative 'host'
 class Game
   def initialize
     @host = Host.new
+    @secret_word_array = @host.choose_random_word.split('')
+    @secret_word_progress_array = Array.new(@secret_word_array.length, '_')
   end
 
   def play
--- lib/host.rb
+++ lib/host.rb
@@ -2,13 +2,6 @@
 
 # Host class
 class Host
-  def initialize
-    @secret_word_array = choose_random_word.split('')
-    @secret_word_progress_array = Array.new(@secret_word_array.size, '_')
-  end
-
-  private
-
   def choose_random_word
     word_bank = []
     file = File.open('google-10000-english-no-swears.txt')
