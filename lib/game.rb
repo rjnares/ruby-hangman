b492049b6a8c763b0364e16ee6192efc334e67bc
Roberto Nares <roberto.nares@gmail.com>
2024-07-25 11:04:01 -0700

    Implement game loop which uses guessed letter to update game state and check for game win/loss scenarios

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,46 @@ class Game
   end
 
   def play
-    letter = next_letter(@secret_word_progress_array, @incorrect_letters, @incorrect_guesses_left)
-    p letter
+    while @incorrect_guesses_left.positive?
+      letter = next_letter(@secret_word_progress_array, @incorrect_letters, @incorrect_guesses_left)
+
+      update_game_state(letter)
+
+      if word_guessed?
+        game_win
+        return
+      end
+
+      game_lose if @incorrect_guesses_left.zero?
+    end
+  end
+
+  private
+
+  def word_guessed?
+    @secret_word_array == @secret_word_progress_array
+  end
+
+  def update_game_state(letter)
+    if correct_letter?(letter)
+      update_progress(letter)
+    else
+      update_incorrect(letter)
+    end
+  end
+
+  def correct_letter?(letter)
+    @secret_word_array.include?(letter)
+  end
+
+  def update_progress(new_letter)
+    @secret_word_array.each_with_index do |letter, index|
+      @secret_word_progress_array[index] = letter if letter == new_letter
+    end
+  end
+
+  def update_incorrect(letter)
+    @incorrect_letters << letter
+    @incorrect_guesses_left -= 1
   end
 end
